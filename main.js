10f96a211a9ead8625471b475129ef4e2f1a3516
vikash_shandilya <52299761+Vikash-shandilya@users.noreply.github.com>
2023-05-02 20:42:15 +0530

    assignment 14

--- main.js
+++ main.js
@@ -1,70 +1,202 @@
-const posts = [
-    { title: 'post one', body: 'this is post 1' },
-    { title: 'post two', body: 'this is post 2' }
-  ];
-  
-  let usertime = [new Date()];
-  
-  function getpost() {
-    setTimeout(() => {
-      console.log(posts)
-      },1000);
-      
-    }
-  
-  
-  function createpost(post) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        posts.push(post);
-        const error = false;
-        if (!error) {
-          resolve();
-        } else {
-          reject('error : something is wrong');
-        }
-      }, 2000);
-    });
-  }
-  
-  function updateLastUserActivityTime() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const newdate= new Date();
-        usertime.push(newdate)
-        resolve();
-      }, 1000);
-    });
-  }
+// GET REQUEST
+function getTodos() {
+  console.log('GET Request');
+  axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')//axios return a promise thats 
+  .then(res=>{//                                                     why we use .then 
+    showOutput(res)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
 
-  function deletepost()
+// POST REQUEST
+function addTodo() {
+  axios.post('https://jsonplaceholder.typicode.com/posts',{title:'this is new title',body:'this is body'})
+  .then((res)=>{
+    showOutput(res)
+  })
+  .catch((err)=>{console.log(err)})
+}
+
+// PUT/PATCH REQUEST(patch just changes the content that we provide but put changes eveything )
+function updateTodo() {
+  axios.patch('https://jsonplaceholder.typicode.com/posts/1',{title:'new one',body:'new body'})
+  .then((res)=>{showOutput(res)})
+  .catch((err)=>{console.log(err)})
+}
+
+// DELETE REQUEST
+function removeTodo() {
+  axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+  .then((res)=>{showOutput(res)})
+  .catch((err)=>{console.log(err)})
+  console.log('DELETE Request');
+}
+
+// SIMULTANEOUS DATA//to get data from different routes simultanouesly
+function getData() {
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/posts'),
+    axios.get('https://jsonplaceholder.typicode.com/photos')])
+  .then(axios.spread((res1,res2)=>{showOutput(res2)}))//read about axios spread
+  .catch((err)=>{console.log(err)})
+}
+
+// CUSTOM HEADERS
+function customHeaders() {
+  const config=
   {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          posts.pop()
-          resolve();
-        }, 2000);
-      });
+    headers:{
+      'ContentType':'some type',
+      Authorization:'some'
+    }
   }
-  
-//   createpost({ title: 'post 3', body: 'yeah this is post 3' })
-//     .then(function () {
-//       console.log(`before creating ${posts[posts.length-1].title}`, usertime);
-      
-//       updateLastUserActivityTime().then(() => {
-//         console.log(`after creating ${posts[posts.length-1].title}`, usertime);
-//       });
-//     });
 
-Promise.all([createpost({ title: 'post 3', body: 'yeah this is post 3' })
-,updateLastUserActivityTime()]).then(()=>{
-    console.log(`before creating ${posts[posts.length-1].title}`,'user activity time'+ usertime[usertime.length-2]);
-    console.log('after creating '+`${posts[posts.length-1].title}`+'user activity time is'+`${usertime[usertime.length-1]}`);
-    getpost();
-    console.log('last activity time of user'+`${usertime[usertime.length-1]}`)
-    deletepost()
-    .then(()=>{
-        console.log(posts)
+  axios.post('https://jsonplaceholder.typicode.com/posts',{title:'new',body:'new_body'},config)
+  .then((res)=>{showOutput(res)})
+  .catch((err)=>{console.log(err)})
+  console.log('Custom Headers');
+}
+
+// TRANSFORMING REQUESTS & RESPONSES
+function transformResponse() {
+  const abc={
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/posts',
+    data:
+    {
+      title:'new one',
+      body:'new_body'
+    },
+    transformResponse:axios.defaults.transformResponse.concat((data)=>{
+      data.title=data.title.toUpperCase()
+      return data
+
     })
-})
+
+  }
+  axios(abc).then((res)=>{showOutput(res)})
+  console.log('Transform Response');
+}
+
+// ERROR HANDLING
+function errorHandling() {
+  axios.get('https://jsonplaceholder.typicode.com/posts/')
+  .then((out)=>{showOutput(out)})
+  .catch((err)=>{
+    if(err.response)//this means response is not in range of 200
+    {
+      console.log(err)
+      console.log(err.response.data)
+      console.log(err.response.headers)
+      console.log(err.response.status)
+
+      if (err.response.status===404)
+      {
+        alert('page not found')
+      }
+
+    }
+    else if (err.request)//means request made but no response 
+    {
+      console.error(err.message)
+    }
+    else
+    {
+      console.error(err.message)
+    }
+  })
+  console.log('Error Handling');
+}
+
+// CANCEL TOKEN
+function cancelToken() {
+  const source = axios.CancelToken.source();
+
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+      cancelToken: source.token
+    })
+    .then(res => showOutput(res))
+    .catch(thrown => {
+      if (axios.isCancel(thrown)) {
+        console.log('Request canceled', thrown.message);
+      }
+    });
+
+  if (true) {
+    source.cancel('Request canceled!');
+  }
+  console.log('Cancel Token');
+}
+
   
+
+
+// INTERCEPTING REQUESTS & RESPONSES
+
+axios.interceptors.request.use((config)=>
+{
+  console.log(`so method ${config.method} is sent at time ${new Date()} `)
+  return config
+},(error)=>{
+  Promise.reject(error)
+}
+  
+)
+
+// AXIOS INSTANCES
+
+const axioinstance=axios.create({
+  baseURL:'https://jsonplaceholder.typicode.com'
+});
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}
+
+// Event listeners
+document.getElementById('get').addEventListener('click', getTodos);
+document.getElementById('post').addEventListener('click', addTodo);
+document.getElementById('update').addEventListener('click', updateTodo);
+document.getElementById('delete').addEventListener('click', removeTodo);
+document.getElementById('sim').addEventListener('click', getData);
+document.getElementById('headers').addEventListener('click', customHeaders);
+document
+  .getElementById('transform')
+  .addEventListener('click', transformResponse);
+document.getElementById('error').addEventListener('click', errorHandling);
+document.getElementById('cancel').addEventListener('click', cancelToken);
